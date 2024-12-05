12a023f12cd779501e4d5d3cc2db6e5624652c42
SarevalasaBharathkumar <sarevalasab@gmail.com>
2024-12-05 13:24:15 +0530

    firebase updated

--- backend/utils/firebaseAdmin.js
+++ backend/utils/firebaseAdmin.js
@@ -1,7 +1,18 @@
 const admin = require('firebase-admin');
 require('dotenv').config();
 
-const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_PATH); // Your service account key
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+};
 
 // Initialize Firebase Admin SDK with the service account
 admin.initializeApp({
